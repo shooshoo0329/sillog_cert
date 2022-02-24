c23f61363fb597615f36bd07a4f23157918134fc
shooshoo0329 <shooshoo329@daum.net>
2022-02-24 12:20:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# A2A SDK JavaScript Tutorial
+
+## 1. 소개
+
+Klip Javascript SDK를 사용하여 Klip App2App 요청을 구현한 간단한 튜토리얼 입니다.
+Klip App2App 요청 및 Klip Javascript SDK에 대한 더 자세한 설명 및 사용 방법은 [개발자 문서](https://docs.klipwallet.com/)를 참고해주세요.
+
+## 2. 시작하기
+
+디렉토리의 파일들을 서버에 올린 후 모바일 환경에서 접속해주세요.
---
+++ index.js
@@ -0,0 +1,61 @@
+const { prepare, request, getResult, getCardList } = klipSDK
+
+// HTML ELEMENTS
+const textArea = document.querySelector('.text-area')
+const rqbtn = document.querySelector('#requestBtn')
+const cfbtn = document.querySelector('#confirmBtn')
+const cert = document.querySelector('.cert')
+const bappName = `Sillog`
+const myaddr = `"0x6e8af0F8E64a0f6a8B0610874b32fd7f3ac6Df18"`
+
+let requestKey
+let address
+
+// HELPER FUNCTIONS
+const setRequestKey = (newRequestKey) => {
+  requestKey = newRequestKey
+  textArea.textContent = `Request Key: ${newRequestKey}`
+}
+
+const setErrorMsg = (errorMsg) => {
+  textArea.textContent = `Error: ${errorMsg}`
+}
+
+const execute = (prepareFunction) => {
+  window.addEventListener("DOMContentLoaded", () => {
+    prepareFunction()
+  })
+}
+
+const authPrepare = async () => {
+  
+  const res = await prepare.auth({ bappName })
+  console.log(res)
+  if (res.err) {
+    setErrorMsg(res.err)
+  } else {
+    setRequestKey(res.request_key)
+  }
+}
+
+const authResult = async () => {
+  const res = await getResult(requestKey)
+  address = JSON.stringify(res.result.klaytn_address)
+  //textArea.textContent = typeof(myaddr)
+  if (address == myaddr) {
+    cert.classList.remove("hidden")
+  } else {
+    textArea.textContent = "noo"
+  }
+}
+
+execute(authPrepare)
+
+rqbtn.addEventListener('click', (e) => {
+  //textArea.textContent = bappName;
+  request(requestKey, () => alert('not mobile'))
+})
+
+cfbtn.addEventListener('click', (e) => {
+  authResult()
+})
